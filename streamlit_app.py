c8037f35b60c3c453f340793b1a3085bfa5a023d
divya-shree1125 <sriyanka1120@gmail.com>
2024-12-15 08:42:08 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,69 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+# import plotly.express as px
+st.set_page_config(page_title='fitness',layout="wide")
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+def page0():
+    st.title(":orange[My fitness Center]")
+    # st.image("GM.jpg",width=950,channels="BGR")
+    left, middle, right,a,b,c,d,e= st.columns(8)
+    if d.button("sign up"):
+        st.session_state.page="page3"
+    if e.button("login"):
+        st.session_state.page="page1"   
+    st.image("GM.jpg",use_container_width=True,channels="BGR")    
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+def page1():
+    st.title("My account")
+    email=st.text_input('Enter the Email Address')
+    password=st.text_input('password',type='password')
+    if st.button("login"):
+        st.session_state.page = "page2"
+    if st.button("cancel"):
+        st.session_state.page = "page0"
 
-st.header(f'GDP in {to_year}', divider='gray')
+def page3():
+    st.title("create account")
+    email=st.text_input('Enter the Email Address')
+    password=st.text_input('password',type='password')
+    username=st.text_input('Enter the unique username')
 
-''
+    if st.button('Create My Account'):
+        # user = auth.create_user(email = email,password=password,uid=username)
+        # st.success("Account created successfully!")
+        st.markdown('Please Login using your email and password')
+        st.balloons()
+    if st.button("login"):
+        st.session_state.page="page1"    
 
-cols = st.columns(4)
+def page2():
+    st.title("welocoom")
+    tab1, tab2, tab3, tab4 = st.tabs([" Home   ","   tacker   ","   plan   ","   food   "])
+    with tab1:
+        st.title("Home")
+        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    with tab2:
+        st.title("Tacker")
+        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    with tab3:
+        st.title("Plan")
+        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    if st.button("Go to Page 1"):
+        st.session_state.page = "page1"
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+if "page" not in st.session_state:
+    st.session_state.page = "page0"
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+if st.session_state.page == "page0":
+    page0()
+elif st.session_state.page == "page1":
+    page1()    
+elif st.session_state.page == "page3":
+    page3()    
+elif st.session_state.page == "page2":
+    page2()  
+    
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+
